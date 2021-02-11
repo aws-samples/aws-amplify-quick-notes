6d26f377c3124aef3dc12bfa8bcd991e8149283e
kkasperek <kt.kasperek@gmail.com>
2021-02-11 17:31:09 -0600

    connect to aws amplify db

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Amplify from "aws-amplify";
 import { AmazonAIPredictionsProvider } from "@aws-amplify/predictions";
 
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import "normalize.css/normalize.css";
 import "./index.css";
 import amplifyConfig from "./aws-exports";
@@ -14,7 +13,3 @@ Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
